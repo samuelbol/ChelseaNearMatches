2cc8e23c65daeefa78a2ca135116f2182a9e243f
SAMUEL BOLARIN <63271806+samuelbol@users.noreply.github.com>
2024-01-28 03:08:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+
+import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pytz import utc
+from keep_alive import keep_alive
+
+keep_alive()
+
+API_URL = "https://footapi7.p.rapidapi.com/api/team/38/matches/near"
+BOT_TOKEN = "6442048834:AAHKikvUOV73P4-_6lk10LlyKQTGWuQqjyA"
+CHAT_ID = "-4057334853"
+BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
+
+API_HEADERS = {
+    "X-RapidAPI-Key": "7b5d884326mshee4e9441f97979ap194f6ejsn29d87abe0656",
+    "X-RapidAPI-Host": "footapi7.p.rapidapi.com"
+}
+
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36"
+}
+
+
+def date_time(time_stamp):
+    return datetime.fromtimestamp(time_stamp, tz=None)
+
+
+def get_match_data():
+    try:
+        response = requests.get(API_URL, headers=API_HEADERS)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions:
+        print(f"Error: Unable to fetch data.")
+        return None
+
+
+def format_match_message(match, match_type):
+    if match:
+        tournament = match["tournament"]["name"]
+        round_info = match["roundInfo"].get("name") if match_type.lower() == "previous" else match["roundInfo"].get(
+            "round", "name")
+        home_team = match["homeTeam"]["shortName"]
+        away_team = match["awayTeam"]["shortName"]
+
+        message = f"🏆 {tournament} - {round_info}\n\n"
+
+        if match_type.lower() == "previous":
+            home_score = match["homeScore"]["current"]
+            away_score = match["awayScore"]["current"]
+            message += f"🏠 *{home_team} {home_score} - {away_score} {away_team}* 🚌"
+
+        if match_type.lower() == "next":
+            start_timestamp = match["startTimestamp"]
+            start_date = date_time(start_timestamp).strftime("%a, %d %b")
+            start_time = date_time(start_timestamp).strftime("%H:%M")
+            message += f"🏠 *{home_team} 🆚 {away_team}* 🚌"
+
+            message += f"\n\n📅 _{start_date}\n⏰ {start_time} (GMT)_"
+
+        return message
+
+    else:
+        return f"No {match_type} match data available."
+
+
+def send_fixture_to_telegram(message):
+    response = requests.post(BASE_URL + "sendMessage",
+                             json={
+                                 "chat_id": CHAT_ID,
+                                 "parse_mode": "Markdown",
+                                 "text": message,
+                             })
+
+    if response.status_code == 200:
+        print("Message sent successfully.")
+    else:
+        print(
+            f"Message sending failed. Status code: {response.text}")
+
+
+def main():
+    match_data = get_match_data()
+
+    if match_data:
+        previous_match_data = match_data.get('previousEvent')
+        next_match_data = match_data.get('nextEvent')
+
+        previous_match = format_match_message(previous_match_data, 'Previous')
+        next_match = format_match_message(next_match_data, 'Next')
+
+        combined_message = f"🔍 *Chelsea's Last Game*\n{previous_match}\n\n---\n\n📅 *Upcoming Fixture*\n{next_match}"
+
+        print(combined_message)
+
+        send_fixture_to_telegram(combined_message)
+
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler(timezone=utc)
+    scheduler.add_job(main, "interval", hours=1)
+    scheduler.start()
+    # main()
